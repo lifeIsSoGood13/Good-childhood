3934c02d988cf6f043db9e0915f018afaa14fb34
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-09-23 17:14:08 +0300

    Add noreferrer to links with target='_blank'

--- src/components/book-details/book-details.js
+++ src/components/book-details/book-details.js
@@ -28,25 +28,25 @@ export function BookDetails() {
           <div className={style.descriptionPricesWrapper}>
             <ul className={style.prices}>
               <li className={style.buttonWrapper}>
-                <a className={style.link} href={book.konz_href} target="_blank">
+                <a className={style.link} href={book.konz_href} target="_blank" rel="noreferrer">
                   <p className={style.buttonKonzeptual}>Купить в &laquo;Концептуале&raquo;</p>
                 </a>
                 <span className={style.price}>{book.konz_price} руб.</span>
               </li>
               <li className={style.buttonWrapper}>
-                <a className={style.link} href={book.wb_href} target="_blank">
+                <a className={style.link} href={book.wb_href} target="_blank" rel="noreferrer">
                   <p className={style.buttonWildberries}>Wildberries</p>
                 </a>
                 <span className={style.price}>{book.wb_price} руб.</span>
               </li>
               <li className={style.buttonWrapper}>
-                <a className={style.link} href={book.ozon_href} target="_blank">
+                <a className={style.link} href={book.ozon_href} target="_blank" rel="noreferrer">
                   <p className={style.buttonOzon}>Ozon</p>
                 </a>
                 <span className={style.price}>{book.ozon_price} руб.</span>
               </li>
               <li className={style.buttonWrapper}>
-                <a className={style.link} href={book.vk_href} target="_blank">
+                <a className={style.link} href={book.vk_href} target="_blank" rel="noreferrer">
                   <p className={style.buttonVkontakte}>ВКонтакте</p>
                 </a>
                 <span className={style.price}>{book.vk_price} руб.</span>
