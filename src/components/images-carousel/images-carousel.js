37302e6a2d95188f722537986c6a7d88a71ec7d2
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-10-02 11:57:12 +0300

    Fix bug in ImagesCarousel component

--- src/components/images-carousel/images-carousel.js
+++ src/components/images-carousel/images-carousel.js
@@ -7,32 +7,45 @@ export function ImagesCarousel({ book }) {
   const booksListData = useContext(BooksListContext);
   const slider = useRef(null);
   const image = useRef(0);
-  const [offset, setOffset] = useState(0);
+  const [currentSize, setCurrentSize] = useState(booksListData.size);
   const [position, setPosition] = useState(1);
 
-  const handlePrevClick = () => {
-    if (offset === 0) {
+  const handlePrevButtonClick = () => {
+    if (position === 1) {
       return;
-    } else {
-      setOffset((currentOffset) => {
-        const newOffset = currentOffset + (image.current.clientWidth + 23);
-        slider.current.style = `transform: translateX(${newOffset}px)`;
-        return newOffset;
-      });
+    } else if (position === 2) {
       setPosition((currentPosition) => currentPosition - 1);
+      slider.current.style = `transform: translateX(0px)`;
+    } else {
+      setPosition((currentPosition) => {
+        const newPosition = currentPosition - 1;
+        const newOffset = (newPosition - 1) * (image.current.clientWidth + 23);
+        slider.current.style = `transform: translateX(-${newOffset}px)`;
+        return newPosition;
+      });
     }
   };
 
-  const handleNextClick = () => {
-    if (offset === -((book.images.length - 1) * (image.current.clientWidth + 23))) {
+  const handleNextButtonClick = () => {
+    if (position === book.images.length) {
       return;
     } else {
-      setOffset((currentOffset) => {
-        const newOffset = currentOffset - (image.current.clientWidth + 23);
-        slider.current.style = `transform: translateX(${newOffset}px)`;
-        return newOffset;
+      setPosition((currentPosition) => {
+        const newPosition = currentPosition + 1;
+        const newOffset = (newPosition - 1) * (image.current.clientWidth + 23);
+        slider.current.style = `transform: translateX(-${newOffset}px)`;
+        return newPosition;
       });
-      setPosition((currentPosition) => currentPosition + 1);
+    }
+  };
+
+  const setImagePosition = () => {
+    if (position === 1) {
+      return;
+    } else {
+      setPosition(1);
+      slider.current.style = `transform: translateX(${0}px)`;
+      setCurrentSize(booksListData.size);
     }
   };
 
@@ -40,6 +53,7 @@ export function ImagesCarousel({ book }) {
     <section className={style.container} aria-label="Иллюстрации из книги">
       <h2 className={style.title}>Иллюстрации</h2>
       <div className={style.window}>
+        {currentSize !== booksListData.size && setImagePosition()}
         <ul className={style.track} ref={slider}>
           {book &&
             book.images.map((item, index) => (
@@ -58,21 +72,21 @@ export function ImagesCarousel({ book }) {
       </div>
       <div className={style.infoWrapper}>
         <button
-          className={offset === 0 ? style.leftButton : classnames(style.leftButton, style.leftButtonActive)}
+          className={position === 1 ? style.leftButton : classnames(style.leftButton, style.leftButtonActive)}
           type="button"
-          onClick={handlePrevClick}
+          onClick={handlePrevButtonClick}
         ></button>
         <span className={style.infoText}>
           {position} / {book && book.images.length}
         </span>
         <button
           className={
-            offset === -((book && book.images.length - 1) * (image.current.clientWidth + 23))
+            book && position === book.images.length
               ? style.rightButton
               : classnames(style.rightButton, style.rightButtonActive)
           }
           type="button"
-          onClick={handleNextClick}
+          onClick={handleNextButtonClick}
         ></button>
       </div>
     </section>
