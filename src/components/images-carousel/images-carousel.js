48f19b8e59ecb5e8202e0fb74370fdbd933a5b82
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-10-02 17:20:41 +0300

    Modified ImagesCarousel component

--- src/components/images-carousel/images-carousel.js
+++ src/components/images-carousel/images-carousel.js
@@ -6,54 +6,46 @@ import { BooksListContext } from '../../services/AppContext';
 export function ImagesCarousel({ book }) {
   const booksListData = useContext(BooksListContext);
   const slider = useRef(null);
-  const image = useRef(0);
-  const [currentSize, setCurrentSize] = useState(booksListData.size);
-  const [position, setPosition] = useState(1);
+  const image = useRef(null);
+  const [initialWindowSize, setInitialWindowSize] = useState(booksListData.size);
+  const [imagePosition, setImagePosition] = useState(1);
+
+  const setImageParams = (position, width, operator) => {
+    setImagePosition((currentPosition) => {
+      const newPosition = operator === 'plus' ? currentPosition + position : currentPosition - position;
+      const newOffset = (newPosition - 1) * width;
+      slider.current.style = `transform: translateX(-${newOffset}px)`;
+      return newPosition;
+    });
+  };
 
   const handlePrevButtonClick = () => {
-    if (position === 1) {
-      return;
-    } else if (position === 2) {
-      setPosition((currentPosition) => currentPosition - 1);
-      slider.current.style = `transform: translateX(0px)`;
-    } else {
-      setPosition((currentPosition) => {
-        const newPosition = currentPosition - 1;
-        const newOffset = (newPosition - 1) * (image.current.clientWidth + 23);
-        slider.current.style = `transform: translateX(-${newOffset}px)`;
-        return newPosition;
-      });
-    }
+    imagePosition === 1 && setImageParams(0, 0, 'plus');
+    imagePosition === 2 && setImageParams(1, 0, 'minus');
+    imagePosition > 2 && setImageParams(1, image.current.clientWidth + 23, 'minus');
   };
 
   const handleNextButtonClick = () => {
-    if (position === book.images.length) {
-      return;
-    } else {
-      setPosition((currentPosition) => {
-        const newPosition = currentPosition + 1;
-        const newOffset = (newPosition - 1) * (image.current.clientWidth + 23);
-        slider.current.style = `transform: translateX(-${newOffset}px)`;
-        return newPosition;
-      });
-    }
+    imagePosition < book.images.length && setImageParams(1, image.current.clientWidth + 23, 'plus');
   };
 
-  const setImagePosition = () => {
-    if (position === 1) {
-      return;
-    } else {
-      setPosition(1);
-      slider.current.style = `transform: translateX(${0}px)`;
-      setCurrentSize(booksListData.size);
-    }
+  const setSliderPosition = (width) => {
+    slider.current.style = `transform: translateX(-${(imagePosition - 1) * width}px)`;
+    setInitialWindowSize(booksListData.size);
+  };
+
+  const selectSliderPosition = () => {
+    imagePosition === 1 && setSliderPosition(0);
+    booksListData.size > 1160 && setSliderPosition(803);
+    booksListData.size > 665 && booksListData.size <= 1160 && setSliderPosition(623);
+    booksListData.size <= 665 && setSliderPosition(316);
   };
 
   return (
     <section className={style.container} aria-label="Иллюстрации из книги">
       <h2 className={style.title}>Иллюстрации</h2>
       <div className={style.window}>
-        {currentSize !== booksListData.size && setImagePosition()}
+        {initialWindowSize !== booksListData.size && selectSliderPosition()}
         <ul className={style.track} ref={slider}>
           {book &&
             book.images.map((item, index) => (
@@ -72,16 +64,18 @@ export function ImagesCarousel({ book }) {
       </div>
       <div className={style.infoWrapper}>
         <button
-          className={position === 1 ? style.leftButton : classnames(style.leftButton, style.leftButtonActive)}
+          className={
+            imagePosition === 1 ? style.leftButton : classnames(style.leftButton, style.leftButtonActive)
+          }
           type="button"
           onClick={handlePrevButtonClick}
         ></button>
         <span className={style.infoText}>
-          {position} / {book && book.images.length}
+          {imagePosition} / {book && book.images.length}
         </span>
         <button
           className={
-            book && position === book.images.length
+            book && imagePosition === book.images.length
               ? style.rightButton
               : classnames(style.rightButton, style.rightButtonActive)
           }
