58cd0d88abb951ec7482dbdc6d6dc017fa95d0d7
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-10-14 17:07:34 +0300

    Add scroll to top after click on link in 404 page

--- src/components/not-found/not-found.js
+++ src/components/not-found/not-found.js
@@ -1,8 +1,12 @@
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import alas from '../../images/alas.jpg';
+import { BooksListContext } from '../../services/AppContext';
 import style from './not-found.module.css';
 
 export function NotFound() {
+  const booksListData = useContext(BooksListContext);
+
   return (
     <div className={style.container}>
       <div className={style.imageContainer}>
@@ -10,7 +14,7 @@ export function NotFound() {
       </div>
       <p className={style.text}>
         А на{' '}
-        <Link className={style.link} to="/">
+        <Link className={style.link} to="/" onClick={booksListData.jumpToTop}>
           главной странице
         </Link>{' '}
         у нас уже кое-что есть!
