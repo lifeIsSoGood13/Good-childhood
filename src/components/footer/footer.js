86ac5f8cd05a2fbe0da4005ebf11e55819c21571
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-02 12:52:43 +0300

    Refactor: move the data in the footer to a constant

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,15 @@
 import Phone from '../../images/phone.svg';
 import Email from '../../images/email.svg';
-import FooterTg from '../../images/footer-telegram.svg';
-import FooterVk from '../../images/footer-vk.svg';
-import FooterYt from '../../images/footer-youtube.svg';
 import style from './footer.module.css';
+import { footerSocialData } from '../../utils/constants';
 
 export function Footer() {
+	const date = new Date().getFullYear();
+
 	return (
 		<footer className={style.footer}>
 			<div className={style.footerWrap}>
-				<p className={style.copyright}>2022&nbsp;&copy; &laquo;Доброе детство&raquo;. Все права защищены</p>
+				<p className={style.copyright}>{date}&nbsp;&copy; &laquo;Доброе детство&raquo;. Все права защищены</p>
 				<div className={style.contacts}>
 					<div className={style.phone}>
 						<img src={Phone} alt='Телефон' />
@@ -21,25 +21,13 @@ export function Footer() {
 					</div>
 				</div>
 				<ul className={style.socialItems}>
-					<li className={style.socialItem}>
-						<a href='https://t.me/dobroe_detstvo' target='_blank' rel='noreferrer'>
-							<img src={FooterTg} alt='Телеграм' />
-						</a>
-					</li>
-					<li className={style.socialItem}>
-						<a href='https://vk.com/izdatel_dobroe_detstvo' target='_blank' rel='noreferrer'>
-							<img src={FooterVk} alt='ВКонтакте' />
-						</a>
-					</li>
-					<li className={style.socialItem}>
-						<a
-							href='https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A'
-							target='_blank'
-							rel='noreferrer'
-						>
-							<img src={FooterYt} alt='Ютуб' />
-						</a>
-					</li>
+					{footerSocialData.map((item, index) => (
+						<li key={index} className={style.socialItem}>
+							<a href={item.socialHref} target='_blank' rel='noreferrer'>
+								<img src={item.socialIcon} alt={item.socialName} />
+							</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</footer>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,55 +1,76 @@
 import telegram from '../images/telegram.svg';
 import vkontakte from '../images/vk.svg';
 import youtube from '../images/youtube.svg';
+import FooterTg from '../images/footer-telegram.svg';
+import FooterVk from '../images/footer-vk.svg';
+import FooterYt from '../images/footer-youtube.svg';
 
 export const headerNavigationData = [
-  { linkName: 'Книги', linkHref: '/books', isHashLink: false },
-  { linkName: 'Видеоконтент', linkHref: '/#video', isHashLink: true },
-  { linkName: 'Контакты', linkHref: '/contacts', isHashLink: false },
+	{ linkName: 'Книги', linkHref: '/books', isHashLink: false },
+	{ linkName: 'Видеоконтент', linkHref: '/#video', isHashLink: true },
+	{ linkName: 'Контакты', linkHref: '/contacts', isHashLink: false },
 ];
 
 export const menuNavigationData = [
-  { linkName: 'Главная', linkHref: '/', isHashLink: false },
-  { linkName: 'Книги', linkHref: '/books', isHashLink: false },
-  { linkName: 'Видеоконтент', linkHref: '/#video', isHashLink: true },
-  { linkName: 'Контакты', linkHref: '/contacts', isHashLink: false },
+	{ linkName: 'Главная', linkHref: '/', isHashLink: false },
+	{ linkName: 'Книги', linkHref: '/books', isHashLink: false },
+	{ linkName: 'Видеоконтент', linkHref: '/#video', isHashLink: true },
+	{ linkName: 'Контакты', linkHref: '/contacts', isHashLink: false },
 ];
 
 export const headerSocialData = [
-  {
-    socialHref: 'https://t.me/dobroe_detstvo',
-    socialIcon: telegram,
-    socialName: 'Телеграм',
-    isYoutube: false,
-  },
-  {
-    socialHref: 'https://vk.com/izdatel_dobroe_detstvo',
-    socialIcon: vkontakte,
-    socialName: 'ВКонтакте',
-    isYoutube: false,
-  },
-  {
-    socialHref: 'https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A',
-    socialIcon: youtube,
-    socialName: 'Ютуб',
-    isYoutube: true,
-  },
+	{
+		socialHref: 'https://t.me/dobroe_detstvo',
+		socialIcon: telegram,
+		socialName: 'Телеграм',
+		isYoutube: false,
+	},
+	{
+		socialHref: 'https://vk.com/izdatel_dobroe_detstvo',
+		socialIcon: vkontakte,
+		socialName: 'ВКонтакте',
+		isYoutube: false,
+	},
+	{
+		socialHref: 'https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A',
+		socialIcon: youtube,
+		socialName: 'Ютуб',
+		isYoutube: true,
+	},
 ];
 
 export const menuSocialData = [
-  {
-    socialHref: 'https://t.me/dobroe_detstvo',
-    socialIcon: telegram,
-    socialName: 'Телеграм',
-  },
-  {
-    socialHref: 'https://vk.com/izdatel_dobroe_detstvo',
-    socialIcon: vkontakte,
-    socialName: 'ВКонтакте',
-  },
-  {
-    socialHref: 'https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A',
-    socialIcon: youtube,
-    socialName: 'Ютуб',
-  },
+	{
+		socialHref: 'https://t.me/dobroe_detstvo',
+		socialIcon: telegram,
+		socialName: 'Телеграм',
+	},
+	{
+		socialHref: 'https://vk.com/izdatel_dobroe_detstvo',
+		socialIcon: vkontakte,
+		socialName: 'ВКонтакте',
+	},
+	{
+		socialHref: 'https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A',
+		socialIcon: youtube,
+		socialName: 'Ютуб',
+	},
+];
+
+export const footerSocialData = [
+	{
+		socialHref: 'https://t.me/dobroe_detstvo',
+		socialIcon: FooterTg,
+		socialName: 'Телеграм',
+	},
+	{
+		socialHref: 'https://vk.com/izdatel_dobroe_detstvo',
+		socialIcon: FooterVk,
+		socialName: 'ВКонтакте',
+	},
+	{
+		socialHref: 'https://www.youtube.com/channel/UCaiMuGF8LARjnHdsUrDRy9A',
+		socialIcon: FooterYt,
+		socialName: 'Ютуб',
+	},
 ];
