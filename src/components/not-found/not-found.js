3bc8a9763659c20a76374e89d6b424ebc5910b76
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-03 17:52:29 +0300

    Feat: add page-not-found image with webp format

--- src/components/not-found/not-found.js
+++ src/components/not-found/not-found.js
@@ -1,24 +1,28 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import alas from '../../images/alas.jpg';
+import alasWebp from '../../images/alas.webp';
 import { BooksListContext } from '../../services/AppContext';
 import style from './not-found.module.css';
 
 export function NotFound() {
-  const booksListData = useContext(BooksListContext);
+	const booksListData = useContext(BooksListContext);
 
-  return (
-    <div className={style.container}>
-      <div className={style.imageContainer}>
-        <img className={style.image} src={alas} alt="Ошибка 404" />
-      </div>
-      <p className={style.text}>
-        А на{' '}
-        <Link className={style.link} to="/" onClick={booksListData.jumpToTop}>
-          главной странице
-        </Link>{' '}
-        у нас уже кое-что есть!
-      </p>
-    </div>
-  );
+	return (
+		<div className={style.container}>
+			<div className={style.imageContainer}>
+				<picture>
+					<source srcset={alasWebp} type='image/webp' />
+					<img className={style.image} src={alas} alt='Ошибка 404' />
+				</picture>
+			</div>
+			<p className={style.text}>
+				А на{' '}
+				<Link className={style.link} to='/' onClick={booksListData.jumpToTop}>
+					главной странице
+				</Link>{' '}
+				у нас уже кое-что есть!
+			</p>
+		</div>
+	);
 }
